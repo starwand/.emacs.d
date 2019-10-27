deeca0d5301654760e13e1c14cfbb13b1c84ca56
starwand <starwand@outlook.jp>
2019-10-27 20:42:12 +0900

    Use git-timemachine and git-gutter-fringe

--- my-packages/my-git.el
+++ my-packages/my-git.el
@@ -1,4 +1,44 @@
 (use-package magit
   :chords (("mg" . magit-status)))
 
+(use-package git-timemachine)
+
+(use-package git-gutter-fringe
+  :diminish git-gutter-mode
+  :config
+  (progn
+    (setq-default left-fringe-width  20)
+    (setq-default right-fringe-width 20)
+    (fringe-helper-define 'git-gutter-fr:added nil
+			  ".XXXXXX."
+			  "XX....XX"
+			  "X......X"
+			  "X......X"
+			  "XXXXXXXX"
+			  "XXXXXXXX"
+			  "X......X"
+			  "X......X")
+    (fringe-helper-define 'git-gutter-fr:deleted nil
+			  "XXXXXX.."
+			  "XX....X."
+			  "XX.....X"
+			  "XX.....X"
+			  "XX.....X"
+			  "XX.....X"
+			  "XX....X."
+			  "XXXXXX..")
+    (fringe-helper-define 'git-gutter-fr:modified nil
+			  "XXXXXXXX"
+			  "X..XX..X"
+			  "X..XX..X"
+			  "X..XX..X"
+			  "X..XX..X"
+			  "X..XX..X"
+			  "X..XX..X"
+			  "X..XX..X")
+    (set-face-foreground 'git-gutter-fr:modified "yellow")
+    (set-face-foreground 'git-gutter-fr:added    "green")
+    (set-face-foreground 'git-gutter-fr:deleted  "magenta")
+    (global-git-gutter-mode t)))
+
 (provide 'my-git)
