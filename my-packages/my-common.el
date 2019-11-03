7b08225c94001cbf024cead3951e4e95c5d2f3a6
starwand <starwand@outlook.jp>
2019-11-03 10:36:48 +0900

    Use know-your-http-well

--- my-packages/my-common.el
+++ my-packages/my-common.el
@@ -8,5 +8,11 @@
 (use-package color-theme-sanityinc-tomorrow
   :config
   (load-theme 'sanityinc-tomorrow-eighties 'no-confirm))
+(use-package know-your-http-well
+  :commands (http-header
+            http-method
+            http-relation
+            http-status-code
+            media-type))
 
 (provide 'my-common)
