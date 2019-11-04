6fb63dfc3c6eee0a513160b7162001ba3f4786d1
starwand <starwand@outlook.jp>
2019-11-05 02:14:35 +0900

    Revert rainbow-delimiters settings to default

--- my-packages/my-rainbow-delimiters.el
+++ my-packages/my-rainbow-delimiters.el
@@ -1,17 +1,5 @@
 (use-package rainbow-delimiters
   :hook
-  (prog-mode . rainbow-delimiters-mode)
-  :config
-  ;; Configure rainbow color nicely: https://qiita.com/gnrr/items/8f9efd5ced058e576f5e
-  (setq rainbow-delimiters-outermost-only-face-count 1)
-  (set-face-foreground 'rainbow-delimiters-depth-1-face "#9a4040")
-  (set-face-foreground 'rainbow-delimiters-depth-2-face "#ff5e5e")
-  (set-face-foreground 'rainbow-delimiters-depth-3-face "#ffaa77")
-  (set-face-foreground 'rainbow-delimiters-depth-4-face "#dddd77")
-  (set-face-foreground 'rainbow-delimiters-depth-5-face "#80ee80")
-  (set-face-foreground 'rainbow-delimiters-depth-6-face "#66bbff")
-  (set-face-foreground 'rainbow-delimiters-depth-7-face "#da6bda")
-  (set-face-foreground 'rainbow-delimiters-depth-8-face "#afafaf")
-  (set-face-foreground 'rainbow-delimiters-depth-9-face "#f0f0f0"))
+  (prog-mode . rainbow-delimiters-mode))
 
 (provide 'my-rainbow-delimiters)
