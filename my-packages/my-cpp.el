260120b3c86380ec5733604f339250bd249ec519
starwand <starwand@outlook.jp>
2020-10-06 18:46:49 +0900

    Fix wrong hook description of google-c-style

--- my-packages/my-cpp.el
+++ my-packages/my-cpp.el
@@ -1,6 +1,6 @@
 (use-package google-c-style
-  :hook ((c-mode-common-hook google-set-c-style)
-         (c-mode-common-hook google-make-newline-indent)))
+  :hook ((c-mode-common . google-set-c-style)
+         (c-mode-common . google-make-newline-indent)))
 
 (use-package clang-format)
 
