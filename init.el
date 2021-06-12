bdc406995ddba2d1c5464c3f868b2b737d8a1d50
starwand <starwand@outlook.jp>
2021-06-12 13:09:26 +0900

    Bootstrap straight.el

--- init.el
+++ init.el
@@ -2,3 +2,17 @@
 ;;; See `run.sh`.
 (setq user-init-file (or load-file-name (buffer-file-name)))
 (setq user-emacs-directory (file-name-directory user-init-file))
+
+;;; Bootstrap straight.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
