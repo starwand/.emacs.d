2b060d9911eaa83a4552217d62c3fc420865a341
starwand <starwand@outlook.jp>
2021-06-12 21:01:08 +0900

    Add my-smart-mode-line

---
+++ my-packages/my-smart-mode-line.el
@@ -0,0 +1,11 @@
+(require 'use-package)
+
+(use-package smart-mode-line
+  :config
+  (setq sml/no-confirm-load-theme t)
+  (setq sml/theme 'dark)
+  (sml/setup))
+
+(use-package smart-mode-line-powerline-theme)
+
+(provide 'my-smart-mode-line)
