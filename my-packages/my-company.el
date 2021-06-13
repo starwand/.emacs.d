850c36fda946ecb7dd9fbb88737829402d709c7e
starwand <starwand@outlook.jp>
2021-06-13 11:18:00 +0900

    Use company-indent-or-complete-common with prog-mode

--- my-packages/my-company.el
+++ my-packages/my-company.el
@@ -2,14 +2,21 @@
 
 (use-package company
   :diminish company-mode
+  :init
+  (add-hook 'prog-mode-hook
+            #'(lambda ()
+                (local-set-key (kbd "<tab>")
+                               #'company-indent-or-complete-common)))
   :config
   (global-company-mode)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous)
+  (define-key company-search-map (kbd "C-n") 'company-select-next)
+  (define-key company-search-map (kbd "C-p") 'company-select-previous)
   (setq company-idle-delay 0.2
         company-minimum-prefix-length 2
         company-tooltip-limit 20
         company-selection-wrap-around t)
-  (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
-  (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
   (set-face-attribute 'company-tooltip nil
                       :foreground "black"
                       :background "grey80")
