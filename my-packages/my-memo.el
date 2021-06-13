97bcc598e12e2836785659f6f2a90a0838c8b145
starwand <starwand@outlook.jp>
2021-06-13 16:53:41 +0900

    Bind hydra-deft with "?" in deft-mode-map

--- my-packages/my-memo.el
+++ my-packages/my-memo.el
@@ -2,6 +2,20 @@
 
 (use-package deft
   :defer t
-  :bind ("C-, C-," . deft))
+  :bind (("C-, C-," . deft)
+         :map deft-mode-map
+         ("?" . hydra-deft/body))
+  :config
+  (setq deft-directory "~/work/memo"
+        deft-extensions '("md")
+        deft-recursive t
+        deft-new-file-format "%Y%m%d-%H%M%S")
+  (defvar deft-default-extension (car deft-extensions))
+  :hydra
+  (hydra-deft (:color teal :hint nil :columns 4)
+  "deft"
+  ("n" deft-new-file       "New Memo")
+  ("N" deft-new-file-named "New Memo (named)")
+  ("q" nil "Quit" :color blue)))
 
 (provide 'my-memo)
