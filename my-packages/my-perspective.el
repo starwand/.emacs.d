3cb2d9e336661f259db36827c7e403d8c06c771a
starwand <starwand@outlook.jp>
2021-06-16 00:05:57 +0900

    Load persp-state-file only when the file exists

--- my-packages/my-perspective.el
+++ my-packages/my-perspective.el
@@ -8,7 +8,8 @@
   (global-set-key (kbd "C-<prior>") 'persp-prev)
   (global-set-key (kbd "C-<next>") 'persp-next)
   (setq persp-state-default-file (concat (file-name-directory user-init-file) "persp-state-file"))
-  (persp-state-load persp-state-default-file)
+  (if (file-exists-p persp-state-default-file)
+      (persp-state-load persp-state-default-file))
   (add-hook 'kill-emacs-hook #'persp-state-save))
 
 (provide 'my-perspective)
